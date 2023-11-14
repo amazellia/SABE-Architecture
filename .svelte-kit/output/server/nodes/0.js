288743a62b99368d4848b476fa837102f68d95c7
Amanda Patricia Viray <71745412+amazellia@users.noreply.github.com>
2023-11-15 09:35:11 +1100

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.9cf967db.js","_app/immutable/chunks/scheduler.012baebd.js","_app/immutable/chunks/index.9f01cf0a.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.a4c65673.js","_app/immutable/chunks/scheduler.012baebd.js","_app/immutable/chunks/index.9f01cf0a.js","_app/immutable/chunks/singletons.1cdf1f32.js"];
-export const stylesheets = [];
-export const fonts = [];

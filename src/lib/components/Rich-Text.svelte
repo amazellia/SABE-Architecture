23414adbd5a95180e2683c0ad0676eaf80d80a98
Amanda Patricia Viray <71745412+amazellia@users.noreply.github.com>
2024-03-21 00:51:37 +0000

    Changing favicon and fixing rich-text

--- src/lib/components/Rich-Text.svelte
+++ src/lib/components/Rich-Text.svelte
@@ -1,9 +1,13 @@
 <script>
     import { storyblokEditable, renderRichText } from '@storyblok/svelte';
+
     export let blok;
+
     $: resolvedRichText = renderRichText(blok.body);
 </script>
-<div use:storyblokEditable={blok}>
+
+<div use:storyblokEditable={blok} 
+class=" w-full">
     <div class="grid justify-items-center mx-auto mb-12">
         <div class="w-2/3 prose">{@html resolvedRichText}</div>
     </div>

84479ddc816cd6de305d3f78193f44c83ac1b755
Amanda Patricia Viray <71745412+amazellia@users.noreply.github.com>
2024-03-26 23:13:47 +0000

    fixing ProjectList

--- src/lib/components/ProjectList.svelte
+++ src/lib/components/ProjectList.svelte
@@ -8,8 +8,17 @@
     let hasMorePages = true; // Flag to check if there are more pages
     const perPage = 12; 
     let project = [];
+    let yearList = [];
+    let selectYear = [];
     
     const loadPage = async () => {
+        const {year} = storyblokApi.get('cdn/stories/config/', {})
+        .then(response => {
+            yearList = response.data.story.content.year;
+            return yearList;
+        }).catch(error => {
+            console.log(error);
+    });       
 
         const { data } = await storyblokApi.get('cdn/stories', {
             version: 'published',
@@ -55,12 +64,30 @@
             window.scrollTo({ top: 0, behavior: 'smooth' });
         };
 
+    const handleYearSelection = (event) => {
+    selectYear  = event.target.value;
+        if (selectYear  == "all") {
+            // If "all" is selected, add all years to the array
+            selectYear = [...yearList.filter((x) => x !== "all")].toString();
+        }
+    currentPage = 1; // Reset to the first page when changing the filter
+    };
+
 </script>
 
 <div class="py-24"> 
+    <div class="mt-4 text-center">
+        <label for="yearSelector" class="block text-gray-700">Select Year:</label>
+        <select id="yearSelector" class="mt-1 p-2 border rounded" on:change={handleYearSelection}>
+            {#each yearList as year}
+                <option value={year}>{year}</option>
+            {/each}
+        </select>
+        <input type="submit" value="Submit" on:click={loadPage}>
+    </div>
 
     <div class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start">
-        {#each project as p}
+        {#each peri as p}
             <ProjectCard peri={p.content} slug={p.full_slug} />
         {/each}
     </div>

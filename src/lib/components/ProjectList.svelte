bb2a8016952096dc45fc8b5b837c82281ee3b663
Amanda Patricia Viray <71745412+amazellia@users.noreply.github.com>
2024-03-27 00:27:21 +0000

    fixing length for centering grid

--- src/lib/components/ProjectList.svelte
+++ src/lib/components/ProjectList.svelte
@@ -94,7 +94,7 @@
 
     <div class="container justify-center mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start ">
         {#each project as p}
-            <ProjectCard peri={p.content} slug={p.full_slug} pageLength={(length/perPage)}/>
+            <ProjectCard peri={p.content} slug={p.full_slug} pageLength={length}/>
         {/each}
     </div>
 

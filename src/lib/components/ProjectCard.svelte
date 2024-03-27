30e51ba65492d95bf346baab5037cb4fcee2faa8
Amanda Patricia Viray <71745412+amazellia@users.noreply.github.com>
2024-03-27 01:22:06 +0000

    testing centering grid

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -4,24 +4,25 @@
     export let slug;
 </script>
 
-<a
+<div class="col-span-full={length === 1}" use:storyblokEditable={peri}>
+    <a
     href="/{slug}"
-    use:storyblokEditable={peri}
     class="w-full h-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden"
->
-    <img
-        src="{peri.mainImage?.filename}/m/600x0"
-        alt={peri.mainImage?.alt}
-        class="w-full h-48 xl:h-72 object-cover pointer-events-none"
-    />
-    <div class="p-4">
-        <h3 class="text-xl font-bold mb-3">
-            {peri.projectName}
-        </h3>
-        {#if peri.CreatorsName}
-        <div class="line-clamp-4">
-            {peri?.CreatorsName}
+    >
+        <img
+            src="{peri.mainImage?.filename}/m/600x0"
+            alt={peri.mainImage?.alt}
+            class="w-full h-48 xl:h-72 object-cover pointer-events-none"
+        />
+        <div class="p-4">
+            <h3 class="text-xl font-bold mb-3">
+                {peri.projectName}
+            </h3>
+            {#if peri.CreatorsName}
+            <div class="line-clamp-4">
+                {peri?.CreatorsName}
+            </div>
+            {/if}
         </div>
-        {/if}
-    </div>
-</a>
+    </a>
+</div>
--- src/lib/components/ProjectList.svelte
+++ src/lib/components/ProjectList.svelte
@@ -92,9 +92,9 @@
     </div>
     {/if}
 
-    <div class="container justify-center mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start col-span-full={length === 1}">
+    <div class="container justify-center mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start">
         {#each project as p}
-            <ProjectCard peri={p.content} slug={p.full_slug}/>
+            <ProjectCard peri={p.content} slug={p.full_slug} length={length}/>
         {/each}
     </div>
 

cb989decda8367e0098b76f10bab4dab18c362b5
Amanda Viray <amandaviray@yahoo.com>
2024-08-09 15:35:59 +1000

    Update Project.svelte

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -12,23 +12,29 @@
     <div class="p-2 w-full text-center grid col-span-1">
         <h1 class="text-sm"><HeadlineColorful headline={blok.projectName}/></h1>
         <h2 class="text-xl lg:text-3xl  font-bold mt-12 mb-4 text-center">by {blok.CreatorsName}</h2>
-        <a href="/{blok?.project_tutorial.full_slug}"> {blok?.project_tutorial.full_slug.name}</a>
-        <a href="/{blok?.course_event.full_slug}"> {blok?.course_event.full_slug.name}</a>
-        <a href="/{blok?.exhibit_event.full_slug}"> {blok?.exhibit_event.full_slug.name}</a>
         <hr>
     </div>
     
     <div class="grid grid-col md:grid-row md:grid-cols-2 w-dvw">
         <div class="w-full m-4">
             <img
-            src="{blok.mainImage.filename}/m/1600x0"
-            alt={blok.mainImage.alt}
+            src="{blok?.mainImage.filename}/m/1600x0"
+            alt={blok?.mainImage.alt}
             class="w-full h-3/4 object-contain"
             />
+            {#if blok?.project_tutorial}
+            <a href="/{blok?.project_tutorial.full_slug}"> {blok?.project_tutorial.full_slug.name}</a>
+            {/if}
+            {#if blok?.course_event}
+            <a href="/{blok?.course_event.full_slug}"> {blok?.course_event.full_slug.name}</a>
+            {/if}
+            {#if blok?.exhibit_event}
+            <a href="/{blok?.exhibit_event.full_slug}"> {blok?.exhibit_event.full_slug.name}</a>
+            {/if}
         </div>
 
         <div class="m-1">
-        {#if blok.date}
+        {#if blok?.date}
             <h2 class="text-xl lg:text-2xl text-[#1d243d] font-bold mb-4">
                 {blok.date}
             </h2>
@@ -39,14 +45,7 @@
             <a href="/{tutor.full_slug}"> {tutor.full_slug.name}</a> 
             {/each}
         {/if}
-        
-        {#if blok.guests}
-            {#each blok.guests as guest}
-            <a href="/{guest.full_slug}">
-                {guest.name}
-            </a>
-            {/each}
-        {/if}
+
         <div class="m-3 prose">{@html resolvedRichText}</div>
         </div>
     </div>
